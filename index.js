88e1fac2d5b2a5cb12a9db1550b111c4a91275a4
minced1 <66805829+minced1@users.noreply.github.com>
2021-11-07 16:16:48 +0100

    bug fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.ALL] });
+const client = new Client({ intents: [Intents.FLAGS.GUILD] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
